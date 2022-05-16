caee57c004640040d2e38778f907776df0c3a82a
Aurel1701 <gantzfan96@gmail.com>
2022-05-16 11:14:54 +0200

    add img/logo header and title

---
+++ src/components/FreelancerHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="free">
+    <img class="logo" src="../assets/logo.png" alt="" />
+    <div class="text-free">
+        <h4 style="color:lightgray">Avada Freelancer</h4>
+        <h1>Art Director</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.free {
+  height: 600px;
+  background-image: url("../assets/freeheader.jpg");
+  background-position: center;
+  opacity: 0.7;
+}.logo{
+    padding-top:20px;
+    padding-left: 20px;
+}.text-free{
+   text-align: center;
+
+}h1{
+    font-size: 100px;
+    color:red;
+}h4{
+    padding-top: 100px;
+}
+
+</style>
